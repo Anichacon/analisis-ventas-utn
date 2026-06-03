581b8828c17fcdcdde669156aec5f41ed63aea71
Anichacon <analia.s.chacon@gmail.com>
2026-06-03 10:18:16 -0300

    TP2-3: Implementar análisis de ventas

--- scripts/analisis_ventas.py/analisisventas.py
+++ scripts/analisis_ventas.py/analisisventas.py
@@ -1,1 +1,13 @@
-print("hola")
+import pandas as pd
+
+ventas = pd.read_excel("datos/dataset.xlsx")
+
+print(ventas)
+
+ventas_totales = (ventas["Cantidad"] * ventas["Precio"]).sum()
+
+print("Ventas totales:", ventas_totales)
+
+producto_mas_vendido = ventas["Producto"].mode()[0]
+
+print("Producto más vendido:", producto_mas_vendido)
